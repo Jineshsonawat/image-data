019ccef916f10ae81b736a9c39c60dac4e3b72d0
dharmeshKochariOS <jineshsonawat9929@gmail.com>
2024-01-20 18:04:17 +0530

    feat & files: Added api call and show data on search.

--- src/context/main-context.js
+++ src/context/main-context.js
@@ -6,14 +6,43 @@ const mainContext = createContext({ value: false });
 function MainProvider({ children }) {
   const [booleanValue, setBooleanValue] = useState(true);
   const [userValue, setUserValue] = useState("");
+  const [showUserValue, setShowUserValue] = useState("");
+  const [imageDataFromApi, setImageDataFromApi] = useState([]);
+  const [loader, showLoader] = useState("");
 
   function onClickSearchButton() {
-    setBooleanValue(!booleanValue);
+    showLoader("loading");
+    setBooleanValue(false);
+    setShowUserValue(userValue);
+    setUserValue("");
+
+    const key = "41913448-6fe0696dc3241e44a8839e52f";
+    const url =
+      "https://pixabay.com/api/" +
+      `?key=${key}` +
+      `&q=${userValue}` +
+      `&per_page=6`;
+
+    console.log(url);
+    try {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setImageDataFromApi(data.hits);
+          showLoader("success");
+        })
+        .catch((err) => {
+          showLoader("failed");
+        });
+    } catch (error) {
+      showLoader("failed");
+    }
   }
 
   function onChangeinputValue(event) {
     setUserValue(event.target.value);
   }
+
   return (
     <mainContext.Provider
       value={{
@@ -21,6 +50,9 @@ function MainProvider({ children }) {
         userValue,
         onClickSearchButton,
         onChangeinputValue,
+        showUserValue,
+        imageDataFromApi,
+        loader,
       }}
     >
       {children}
